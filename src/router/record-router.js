333754b80e170fa210abbe8740243315924352cb
GoodCodeSeeker <45922417+GoodCodeSeeker@users.noreply.github.com>
2020-11-11 18:44:18 -0500

    w2

--- src/router/record-router.js
+++ src/router/record-router.js
@@ -102,7 +102,7 @@ router.put('/:id', async (req, res) => {
         
         res.json({
             status: true,
-            mensaje: 'Editado'
+            message: 'Edited'
         })
 
     } catch (error) {
@@ -110,7 +110,7 @@ router.put('/:id', async (req, res) => {
 
         res.json({
             status: false,
-            mensaje: 'No editado'
+            message: 'Not edited'
         })
 
     }
